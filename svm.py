d29d7575dc09d166e87c2a15f999c3c3d5dbded8
Denholm Scrimshaw <scrimshaw_is_at@hotmail.com>
2016-03-29 17:30:58 -0700

    Added reporting and randomization

--- svm.py
+++ svm.py
@@ -3,6 +3,7 @@ import numpy as np
 import csv_file_reader
 from sklearn import svm
 from sklearn.cross_validation import KFold
+from sklearn.metrics import classification_report
 
 #TODO Incorporate artist somehow - maybe add it to a list and include a 'count' for the amount of itmes included
 def constructData(dataset, binaryClass):
@@ -19,14 +20,18 @@ def constructData(dataset, binaryClass):
 
 def performKFold(name, svc, n_folds):
 	scores = []
-	fold = KFold(len(categories), n_folds)
+	target_names = ['dislikes', 'likes']
+	fold = KFold(len(randomizedCategories), n_folds)
 
 	for k, (train, test) in enumerate(fold):
 		print ("Fold %d: " % k)
-		svc.fit(data[train], categories[train])
+		svc.fit(randomizedData[train], randomizedCategories[train])
 		print ("Predictions: ")
-		print (svc.predict(data[test]))
-		scores.append(svc.score(data[test], categories[test]))
+		prediction =  svc.predict(randomizedData[test])
+		print (prediction)
+		scores.append(svc.score(randomizedData[test], randomizedCategories[test]))
+		report = classification_report(randomizedCategories[test], prediction, target_names=target_names)
+		print(report)
 
 	print (name + ": " + str(scores))
 	print ("Mean: " + str(np.mean(scores)))
@@ -37,20 +42,31 @@ categories = []
 constructData(csv_file_reader.data_set, 1)
 constructData(csv_file_reader.dislike_data_set, 0)
 
-data = np.array(data)
-categories = np.array(categories)
+randomizedData = []
+randomizedCategories = []
+
+index_list = list(range(len(data)))
+random.shuffle(index_list)
+for index in index_list:
+	randomizedData.append(data[index])
+	randomizedCategories.append(categories[index])
+
+randomizedData = np.array(randomizedData)
+randomizedCategories = np.array(randomizedCategories)
 
 
 C = 1.0 										# SVM regularization parameter
 k_folds = 10									# Number of cross validation folds
 svc = svm.SVC(kernel='linear', C=C)
 lin_svc = svm.LinearSVC(C=C)
-rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C)
-#poly_svc = svm.SVC(kernel='poly', degree=3, C=C) # This pauses execution on lin_svc fold 8 for some reason
+rbf_svc = svm.SVC(kernel='rbf', C=C)
+nu_svc = svm.NuSVC()
+#sigmoid_svc = svm.SVC(kernel='sigmoid', C=C)
 
 performKFold('svc', svc, k_folds)
 performKFold('lin_svc', lin_svc, k_folds)
 performKFold('rbf_svc', rbf_svc, k_folds)
-#performKFold('poly_svc', poly_svc, k_folds)
+performKFold('nu_svc', nu_svc, k_folds)
+#performKFold('sigmoid_svc', sigmoid_svc, k_folds)
 
 
