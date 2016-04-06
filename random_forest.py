cacf92328bcc340ea3118881ee5e8167b5339f34
brittanynolan <nolanbre@gmail.com>
2016-04-06 13:44:38 -0700

    Added Sarah's new song test to random forest

--- random_forest.py
+++ random_forest.py
@@ -13,6 +13,9 @@ data = [[]]
 data_set = []
 target = []
 
+test_songs = []
+test_songs_target = []
+
 with open('spotify_dataset.csv') as file:
     reader = csv.reader(file)
     for row in reader:
@@ -31,6 +34,14 @@ with open('spotify_dislike_dataset.csv') as file:
         # makes a column of 0s since these are 'liked' tracks
         target.append(0)
 
+with open('spotify_test_dataset(classified).csv') as file:
+    reader = csv.reader(file)
+    reader.next()
+    for row in reader:
+        data[0] = row
+        test_songs.append([float(i) for i in data[0][2:-1]]) 
+        test_songs_target.append(float(data[0][-1]))
+
 # groups (data_set, target) so that they can be shuffled together
 zipped = list(zip(data_set, target))
 
@@ -42,35 +53,11 @@ data_set, target = zip(*zipped)
 classifier = RandomForestClassifier()
 
 score = 0
-# size = len(data_set)
-# interval = size / 10
-
-# k = 10
-# folded_data_set = []
-# folded_target = []
 
 classifier.fit(data_set, target)
 
 # predict classification of test data using cross validation
 predicted = cross_validation.cross_val_predict(classifier, data_set, target, cv=10)
-
-# split into k folds
-# for i in range(k):
-#     folded_data_set.append(list(data_set[i * interval:(i + 1) * interval]))
-#     folded_target.append(list(target[i * interval:(i + 1) * interval]))
-
-# for i in range(0, k):
-#     # adding all folds before and after validation fold, e.g. folds 0 to 4 + folds 5 to k
-#     training_X = sum(folded_data_set[0:k - 1 - i], []) + sum(folded_data_set[k - i:k], [])
-#     training_y = sum(folded_target[0:k - 1 - i], []) + sum(folded_target[k - i:k], [])
-
-#     classifier.fit(training_X, training_y)
-
-#     validation_X = folded_data_set[k - 1 - i]
-#     validation_y = folded_target[k - 1 - i]
-
-#     actual += validation_y
-#     np.concatenate(predicted, classifier.predict(validation_X))
 
 accuracy = accuracy_score(target, predicted)
 print ('random forest classifier accuracy = %s' % (accuracy))
@@ -95,4 +82,16 @@ print ('----------------------------------------------------')
 
 target_names = ['dislikes', 'likes']
 report = classification_report(target, predicted, target_names=target_names)
-print (report)
+
+print report
+
+print ('\n')
+print ('----------------------------------------------------')
+print ('random forest - manual song check:')
+print ('----------------------------------------------------')
+
+test_songs_predicted = classifier.predict(test_songs)
+
+report = classification_report(test_songs_target, test_songs_predicted, target_names=target_names)
+
+print report
